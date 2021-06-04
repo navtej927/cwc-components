597c76697b0a2696ef63aa0dc6132eca2f54b553
navtsingh <navtej.singh@move.com>
2021-06-04 13:36:17 -0700

    delete the dist folders

--- packages/card/dist/card.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('Button', () => {
-    it('should produce forty-two', () => {
-        expect(42).toEqual(42);
-    });
-});
-//# sourceMappingURL=card.test.js.map
