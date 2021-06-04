1cef76ff55fb0602959ac85a81f1faaa81e17ae0
navtsingh <navtej.singh@move.com>
2021-06-04 13:04:19 -0700

    card updated

--- packages/card/src/card.tsx
+++ packages/card/src/card.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Button } from '@cwcode/button';
 
-interface CardProps {
-}
-
-export const Card = (props: CardProps) => {
+export const Card = () => {
   return (
     <div>
       CARD
